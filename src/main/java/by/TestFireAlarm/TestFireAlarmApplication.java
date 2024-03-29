ad1b901b80e897c8b84c4ecbfe034f6cc09cf25c
HuzDmitry <guzdima@yandex.by>
2024-03-29 16:52:12 +0300

    удалил лишние зависимости

--- src/main/java/by/TestFireAlarm/TestFireAlarmApplication.java
+++ src/main/java/by/TestFireAlarm/TestFireAlarmApplication.java
@@ -1,13 +1,11 @@
 package by.TestFireAlarm;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.info.Info;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
-@OpenAPIDefinition(info = @Info(title = "API TestFireAlarm", version = "1.0"))
 public class TestFireAlarmApplication {
 
 	public static void main(String[] args) {
